18b51c00c2e8441a9ed5264dadb158e0c20aa150
吉思达 <42016876+jisida@users.noreply.github.com>
2019-11-07 14:13:10 +0800

    upload

--- examples/miniprogram_npm/vtuweapp/loading/vtu-loading.js
+++
@@ -1,6 +0,0 @@
-import { VtuComponent } from '../assets/package/component';
-import { behavior_loading } from '../assets/behaviors/basic';
-VtuComponent({
-  externalClasses: ['custom-class', 'icon-class', 'img-class', 'label-class'],
-  behavior:  behavior_loading
-})
